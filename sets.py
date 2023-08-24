4c7190039ba5454295847264007721072256d9b7
Mohamed Boujnane <boujnanemohamed23@gmail.com>
2023-08-24 22:36:26 +0200

    Update sets.py

--- sets.py
+++ sets.py
@@ -7,7 +7,7 @@ s.add(1)
 s.add(2)
 s.add(3)
 s.add(4)
-s.add(3)#this is not as a list, same element will not be prented again
+s.add(3)#this is not as a list, same element will not be printed again
 
 s.remove(2)
 print (s)
